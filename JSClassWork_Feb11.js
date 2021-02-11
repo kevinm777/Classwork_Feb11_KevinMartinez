9bc1e587d1535f86044576698f6b1704f209bd23
Kevin Martinez Palacios <69471354+kevinm777@users.noreply.github.com>
2021-02-11 11:02:10 -0600

    Add files via upload

---
+++ JSClassWork_Feb11.js
@@ -0,0 +1,12 @@
+function helloWorld(a,b,c)
+{
+  let sum = a+b+c
+ // let a = 10
+  //let b = 50
+  //let c = -100
+  //let sum=a+b+c;
+ // var name="vivian";
+ // console.log("Hello"+ name + "!");
+ console.log(sum);
+}
+helloWorld(10,50,-100)//function call
